6abc932bdd67e56ae45908c94baf095a2a535f29
schuylervianilewis-hash <schuylervianilewis@gmail.com>
2026-09-11 12:04:43 +0530

    [1/15] Deploy vian-voice-notes via Vian Github Help

--- app/src/main/java/com/example/TestCloseable.kt
+++
@@ -1,8 +0,0 @@
-package com.example
-import android.graphics.Bitmap
-import com.facebook.common.references.CloseableReference
-import com.facebook.common.references.ResourceReleaser
-
-fun testRef(bmp: Bitmap): CloseableReference<Bitmap> {
-    return CloseableReference.of(bmp, ResourceReleaser { })
-}
